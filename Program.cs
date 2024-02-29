1541955aada06d10da82b3eae7fa1bc541b69bae
rolandi039 <161744746+rolandi039@users.noreply.github.com>
2024-02-29 20:27:08 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int secretNumber = random.Next(1, 1026);
+
+        int score = 11;
+        int highscore = 0;
+
+        Console.WriteLine("Start guessing...");
+
+        while (true)
+        {
+            Console.Write("Enter your guess: ");
+            string input = Console.ReadLine();
+
+            if (!int.TryParse(input, out int guess))
+            {
+                Console.WriteLine("No number!");
+                continue;
+            }
+
+            if (guess == secretNumber)
+            {
+                Console.WriteLine("Correct Number!");
+                Console.WriteLine($"The secret number was: {secretNumber}");
+
+                if (score > highscore)
+                {
+                    highscore = score;
+                    Console.WriteLine($"New Highscore: {highscore}");
+                }
+
+                break;
+            }
+            else
+            {
+                if (score > 1)
+                {
+                    Console.WriteLine(guess > secretNumber ? "Too high!" : "Too low!");
+                    score--;
+                    Console.WriteLine($"Remaining attempts: {score}");
+                }
+                else
+                {
+                    Console.WriteLine("You lost the game!");
+                    Console.WriteLine($"The secret number was: {secretNumber}");
+                    Console.WriteLine($"Your highscore: {highscore}");
+                    break;
+                }
+            }
+        }
+    }
+}
